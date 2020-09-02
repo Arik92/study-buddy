f824b7443feae554fb42f70cfafdbe388c576ff4
arik92 <arik92@gmail.com>
2020-09-02 11:37:59 +0300

    speech service functional

--- src/app/answer-recorder/answer-recorder.component.ts
+++ src/app/answer-recorder/answer-recorder.component.ts
@@ -21,10 +21,6 @@ export class AnswerRecorderComponent implements OnInit {
     'have a great day',
     'she sells seashells on the seashore'
   ];  
-  // recognition: SpeechRecognition = new Window['SpeechRecognition'];
-  // speechRecognitionList: SpeechGrammarList;
-  phrasePara: any = document.querySelector('.phrase');
-  resultPara: any = document.querySelector('.result');
 
   constructor(private speechService: SpeechRecognitionService) { }
 
@@ -47,95 +43,8 @@ export class AnswerRecorderComponent implements OnInit {
     // this.speechService.addGrammar(grammar);
     this.speechService.record(grammar).subscribe(res => {
       console.log('FINISHED listening.. heard ', res);
+      this.result = res;
     })
   }
-/*
-  testSpeech() {
-    this.status = 'listening';  
-    let targetPhrase = this.phrases[this.selectRandom()];
-    // To ensure case consistency while checking with the returned output text
-    targetPhrase = targetPhrase.toLowerCase();
-    this.target = targetPhrase;  
-  
-    var grammar = '#JSGF V1.0; grammar phrase; public <phrase> = ' + targetPhrase +';';
-    // var recognition = new SpeechRecognition();
-    // var speechRecognitionList = new SpeechGrammarList();
-    this.speechRecognitionList.addFromString(grammar, 1);
-    this.recognition.grammars = this.speechRecognitionList;
-    this.recognition.lang = 'en-US';
-    this.recognition.interimResults = false;
-    this.recognition.maxAlternatives = 1;
-  
-    this.recognition.start();
-  
-    this.recognition.onresult = (event) => {
-      // The SpeechRecognitionEvent results property returns a SpeechRecognitionResultList object
-      // The SpeechRecognitionResultList object contains SpeechRecognitionResult objects.
-      // It has a getter so it can be accessed like an array
-      // The first [0] returns the SpeechRecognitionResult at position 0.
-      // Each SpeechRecognitionResult object contains SpeechRecognitionAlternative objects that contain individual results.
-      // These also have getters so they can be accessed like arrays.
-      // The second [0] returns the SpeechRecognitionAlternative at position 0.
-      // We then return the transcript property of the SpeechRecognitionAlternative object 
-      var speechResult = event.results[0][0].transcript.toLowerCase();
-      this.result = 'Speech received: ' + speechResult + '.';
-      if(speechResult === targetPhrase) {
-        this.result += ': I heard the correct phrase!';
-      } else {
-        this.result += ': That didn\'t sound right.';
-      }
-  
-      console.log('Confidence: ' + event.results[0][0].confidence);
-    }
-  
-    this.recognition.onspeechend = () => {
-      this.recognition.stop();    
-      this.status = 'ready';
-    }
-  
-    this.recognition.onerror = (event: any) => {    
-      this.status = 'ready';
-      this.result = 'Error occurred in recognition: ' + event.error;
-    }
-    
-    this.recognition.onaudiostart = (event: any) => {
-        //Fired when the user agent has started to capture audio.
-        console.log('SpeechRecognition.onaudiostart');
-    }
-    
-    this.recognition.onaudioend = (event: any) => {
-        //Fired when the user agent has finished capturing audio.
-        console.log('SpeechRecognition.onaudioend');
-    }
-    
-    this.recognition.onend = (event: any) => {
-        //Fired when the speech recognition service has disconnected.
-        console.log('SpeechRecognition.onend');
-    }
-    
-    this.recognition.onnomatch = (event: any) => {
-        //Fired when the speech recognition service returns a final result with no significant recognition. This may involve some degree of recognition, which doesn't meet or exceed the confidence threshold.
-        console.log('SpeechRecognition.onnomatch');
-    }
-    
-    this.recognition.onsoundstart = (event: any) => {
-        //Fired when any sound — recognisable speech or not — has been detected.
-        console.log('SpeechRecognition.onsoundstart');
-    }
-    
-    this.recognition.onsoundend = (event: any) => {
-        //Fired when any sound — recognisable speech or not — has stopped being detected.
-        console.log('SpeechRecognition.onsoundend');
-    }
-    
-    this.recognition.onspeechstart = (event: any) => {
-        //Fired when sound that is recognised by the speech recognition service as speech has been detected.
-        console.log('SpeechRecognition.onspeechstart');
-    }
-    this.recognition.onstart = (event: any) => {
-        //Fired when the speech recognition service has begun listening to incoming audio with intent to recognize grammars associated with the current SpeechRecognition.
-        console.log('SpeechRecognition.onstart');
-    }
-  }  */
 }
 
--- src/app/speech.service.ts
+++ src/app/speech.service.ts
@@ -33,11 +33,11 @@ export class SpeechRecognitionService {
                     var transcript = result[0].transcript;
                     if (result.isFinal) {
                         if (result[0].confidence < 0.3) {
-                            console.log("Unrecognized result - Please try again");
+                            // console.log("Unrecognized result - Please try again");
                         }
                         else {
                             term = _.trim(transcript);
-                            console.log("Did you said? -> " + term + " , If not then say something else...");
+                            // console.log("Did you said? -> " + term + " , If not then say something else...");
                         }
                     }
                 }
@@ -58,14 +58,60 @@ export class SpeechRecognitionService {
           console.log("Say something - We are listening !!!");
       });
   }
-
-  // addGrammar(grammar: string) {
-  //   this.speechRecognition.grammars.addFromString(grammar, 1);
-  // }
-
+  
   DestroySpeechObject() {
       if (this.speechRecognition)
           this.speechRecognition.stop();
   }
-
 }
+/**
+ * recognition hooks reference
+ * this.recognition.onspeechend = () => {
+      this.recognition.stop();    
+      this.status = 'ready';
+    }
+  
+    this.recognition.onerror = (event: any) => {    
+      this.status = 'ready';
+      this.result = 'Error occurred in recognition: ' + event.error;
+    }
+    
+    this.recognition.onaudiostart = (event: any) => {
+        //Fired when the user agent has started to capture audio.
+        console.log('SpeechRecognition.onaudiostart');
+    }
+    
+    this.recognition.onaudioend = (event: any) => {
+        //Fired when the user agent has finished capturing audio.
+        console.log('SpeechRecognition.onaudioend');
+    }
+    
+    this.recognition.onend = (event: any) => {
+        //Fired when the speech recognition service has disconnected.
+        console.log('SpeechRecognition.onend');
+    }
+    
+    this.recognition.onnomatch = (event: any) => {
+        //Fired when the speech recognition service returns a final result with no significant recognition. This may involve some degree of recognition, which doesn't meet or exceed the confidence threshold.
+        console.log('SpeechRecognition.onnomatch');
+    }
+    
+    this.recognition.onsoundstart = (event: any) => {
+        //Fired when any sound — recognisable speech or not — has been detected.
+        console.log('SpeechRecognition.onsoundstart');
+    }
+    
+    this.recognition.onsoundend = (event: any) => {
+        //Fired when any sound — recognisable speech or not — has stopped being detected.
+        console.log('SpeechRecognition.onsoundend');
+    }
+    
+    this.recognition.onspeechstart = (event: any) => {
+        //Fired when sound that is recognised by the speech recognition service as speech has been detected.
+        console.log('SpeechRecognition.onspeechstart');
+    }
+    this.recognition.onstart = (event: any) => {
+        //Fired when the speech recognition service has begun listening to incoming audio with intent to recognize grammars associated with the current SpeechRecognition.
+        console.log('SpeechRecognition.onstart');
+    }
+ */
