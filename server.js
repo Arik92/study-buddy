997c6407d544202029ad8e04ae07c713539251ed
arik92 <arik92@gmail.com>
2020-10-17 17:58:09 +0300

    server routing fix attempt 2

--- server.js
+++ server.js
@@ -1,10 +1,9 @@
 // require('dotenv').config();
 const express = require('express');
 const app = express();
+const staticRoot = __dirname + '/src/';
 
 app.use(express.static(__dirname + '/dist/study-buddy'));
-// app.use(expressSession({ secret: 'Gotta go fast!',
-//  resave: true, saveUninitialized: true,
 
 // app.use(express.static(__dirname + '/src'));
 // app.use(express.static(__dirname + '/src/'+'index.html'));
@@ -12,13 +11,11 @@ app.use(express.static(__dirname + '/dist/study-buddy'));
 // console.log('testing env? ', process.env.NODE_ENV);
 // app.use(express.static(__dirname + '/src'));
 
-app.get('/', function(req, res) {
-  console.log('triggered /* route. dirName is ', __dirname);
-    res.sendFile('src/index.html', {root: path.dirname(__dirname+'/study-buddy/')})
-  });
 // Start the app by listening on the default or Heroku port
 
 // app.use(express.static(staticRoot + 'index.html'));
+app.use(express.static(staticRoot));
+app.use(express.static(__dirname + '/src'));
 
 
 app.listen(process.env.PORT || 8000, () => {
